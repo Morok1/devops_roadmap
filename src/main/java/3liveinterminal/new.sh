5d42ca7a427af8a15e0fbb41cd87336927044827
zhenyasmagin@yandex.ru <zhenyasmagin@yandex.ru>
2019-07-25 11:25:21 +0300

    bash --versiobn

--- src/main/java/3liveinterminal/new.sh
+++ src/main/java/3liveinterminal/new.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-echo "This is information provided by mysystem.sh.  Program starts now."
-
-printf "Today's date is `date`, this is week `date +"%V"`.\n\n"
-
-w | cut -d " " -f 1 - | grep -v USER | sort -u
-
-echo " This is `uname`"
+bash --version
